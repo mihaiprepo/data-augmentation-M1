486d19e8c1c1924c1f2c4bd6777861210d0c7c4f
Mihai <mihai.paul@cpp.canon>
2020-10-19 23:54:05 +0300

    implemented 3 augmentation methods

--- data_aug_m1.py
+++ data_aug_m1.py
@@ -8,13 +8,14 @@ import os
 from tkinter import filedialog, Tk
 from image_augmentation import ImageAugmentation
 
-
 class DataAugmentation:
+
     def __init__(self):
         self.aug_config_dict = {}
         self.input_images_dir_path = ''
         self.output_dir_path = ''
         self.img_count = 1
+        self.img_augmentator = ImageAugmentation()
         # Configure tkinter library
         self.root = Tk() # create Tkinter object
         self.root.attributes("-topmost", True) # place the file explorer as the top most window
@@ -44,25 +45,97 @@ class DataAugmentation:
             os.mkdir(self.output_dir_path)
         os.chdir(self.output_dir_path)
 
+    def extract_rotation_params(self,rotation_params_dict):
+        """
+            iterate through all the parameters of the method and extract the valid rotate parameters
+        """
+        angle = 0
+        for param_name, param_value in rotation_params_dict.items():
+            if(param_name == 'angle' and type(param_value) is int or type(param_value) is float):
+                angle = param_value
+        # print('paran_name',param_name ,angle)
+        return angle
+
+    def extract_flip_params(self,flip_params_dict):
+        """
+            iterate through all the parameters of the method and extract the valid flip parameters
+        """
+        flip_code = 0
+        for param_name, param_value in flip_params_dict.items(): 
+            if(param_name == 'flip_code' and type(param_value) is int):
+                flip_code = param_value
+        return flip_code
+
+    def extract_shift_params(self,shift_params_dict,img_shape_dim):
+        """
+            iterate through all the parameters of the method and extract the valid shift parameters
+        """
+        axis = None
+        shift_range = 0
+        for param_name, param_value in shift_params_dict.items(): 
+            if(param_name == 'axis' and type(param_value) is int and param_value<img_shape_dim):
+                axis = param_value
+            if(param_name == 'shift_range' and type(param_value) is float and param_value<=1): # because it's a probability
+                shift_range = param_value
+        return (axis,shift_range)
+
+    def save_img(self,image,name_content_list):
+        name_content_list = [str(value) for value in name_content_list]
+        img_file_name = '_'.join(name_content_list)+'.jpg'
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        cv2.imwrite(img_file_name, image)
+        self.img_count+=1
+
+    def augment_images(self):
+        img_file_names = os.listdir(self.input_images_dir_path)
+        print(self.aug_config_dict)
+        for current_img_file_name in img_file_names: #iterate through input images
+            current_img_path = os.path.join(self.input_images_dir_path,current_img_file_name)
+            current_img = cv2.imread(current_img_path)
+            current_img = cv2.cvtColor(current_img, cv2.COLOR_BGR2RGB)
+            for _,aug_method_dict in self.aug_config_dict.items(): #iterate trough all the functions of the config file
+                print(aug_method_dict)
+                for method_name,method_prams_dict in aug_method_dict.items(): #iterate through all the methods of each function
+                    if(method_name == 'rotation'):
+                        in_angle = self.extract_rotation_params(method_prams_dict)
+                        rotated_img = self.img_augmentator.rotate_image(current_img,angle=in_angle)
+                        self.save_img(rotated_img,[current_img_file_name.split('.')[0],method_name,self.img_count])
+
+                    if(method_name == 'flip'):
+                        in_flip_code = self.extract_flip_params(method_prams_dict)
+                        fliped_img = self.img_augmentator.flip_image(current_img,flip_code=in_flip_code)
+                        self.save_img(fliped_img,[current_img_file_name.split('.')[0],method_name,self.img_count])
+
+                    if(method_name == 'shift'):
+                        in_axis,in_shift_range = self.extract_shift_params(method_prams_dict,current_img.ndim)
+                        fliped_img = self.img_augmentator.shift_image(current_img,axis=in_axis,shift_range=in_shift_range)
+                        self.save_img(fliped_img,[current_img_file_name.split('.')[0],method_name,self.img_count])
+        
     def apply_augmentations(self):
         cwd = os.getcwd() #to get the current directory as a string
         img_path = os.path.join(cwd,'my_cat.jfif')
         image = cv2.imread(img_path,cv2.IMREAD_UNCHANGED)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         ia = ImageAugmentation()
-        # shift = int(0.2*image.shape[0])
 
-        # for i in range(image.shape[0] -1, image.shape[0] - shift, -1):
-        #     image = np.roll(image, -1, axis=0)
-        #     image[-1, :] = 0
-        res = ia.shift_image(image,axis=0,shift_range=0.3)
+        # res = ia.rotate_image(image,angle=-30.75)
+        # res = ia.shear_image(image,-0.2)
+        # ia.tint_image(image)
+        # res = ia.shift_image(image,axis=1,shift_range=0.2)
+        res= ia.flip_image(image,10.23)
+        # res =self.cv2_clipped_zoom(image,1.3)
         # print(res)
+        # res = ia.random_crop_image(image,int(image.shape[0]*0.59),int(image.shape[1]*0.59))
         plt.imshow(res)
         plt.show()
 
 da = DataAugmentation()
-da.apply_augmentations()
-# da.load_config_file()
-# da.choose_input_dir()
+# da.apply_augmentations()
+da.load_config_file()
+da.choose_input_dir()
+da.augment_images()
+
+
 
 
 # file_path = filedialog.askopenfilename(parent=root)
--- image_augmentation.py
+++ image_augmentation.py
@@ -1,18 +1,20 @@
 import numpy as np
 from cv2 import cv2
+from skimage import transform as sk_transform
+
 class ImageAugmentation:
     def __init__(self):
         pass
 
     def rotate_image(self, image, angle):
         """
-            Rotates the given image by the input angle clockwise
+            Rotates the given image by the input angle in the counter-clockwise direction
             Parameters
             ----------
                 image : ndim np.array
                     image to be rotated
-                angle : int
-                    angle of rotation.
+                angle : float
+                    angle of rotation as degrees.
             Returns
             -------
                 rotated image as np.array
@@ -54,24 +56,96 @@ class ImageAugmentation:
                     image to be shifted.
                 axis : int 
                     Axis along which elements are shifted.
-                shift_range : int
-                    the percentage of the image to be shifted.
+                shift_range : float
+                    the shift range as probability of the given image to be shifted.
             Returns
             -------
                 shited image as np.array
         """
-        # for i in range(image.shape[axis] -1, image.shape[axis] - shift_range, -1):
-        #     result = np.roll(image, -1, axis=axis)
-        #     result[-1, :] = 0
+        shift = int(shift_range*image.shape[axis]) # get the shift range in pixels
+        for _ in range(image.shape[axis] - 1, image.shape[axis] - shift, -1):
+            image = np.roll(image, -1, axis=axis)
+            if (axis == 0):
+                image[-1, :] = 0
+            else:
+                image[:, -1] = 0
+        return np.copy(image)
 
-        print(axis,shift_range)  
-        shift = int(shift_range*image.shape[axis])
-     
-        for i in range(image.shape[axis] -1, image.shape[axis] - shift, -1):
-            result = np.roll(image, -1, axis=axis)
-            result[-1, :] = 0
-        print(result[-1])
+    def shear_image(self,image,shear_angle):
+        """
+            Applies shearing to the given image 
+            Parameters
+            ----------
+                image : ndim np.array
+                    image to be sheared.
+                shear_angle : float
+                    Shear angle in counter-clockwise direction as radians.
+            Returns
+            -------
+                sheared image as np.array
+        """
+        # Create Afine transform
+        afine_tf = sk_transform.AffineTransform(shear=shear_angle)
+        # Apply transform to image data
+        result = sk_transform.warp(image, inverse_map=afine_tf)
         return result
 
-ia = ImageAugmentation()
-# ia.rotate_image(np)
+    def clipped_zoom_image(self,image, zoom_factor):
+        """
+        Center zoom in/out of the given image and returning an enlarged/shrinked view of 
+        the image without changing dimensions
+              Parameters
+            ----------
+                image : ndim np.array
+                    image to be zoomed.
+                zoom_factor : 
+                    amount of zoom as a ratio (0 to Inf)        
+            Returns
+            -------
+                zoomed image as np.array
+        """
+        height, width = image.shape[:2] # It's also the final desired shape
+        new_height, new_width = int(height * zoom_factor), int(width * zoom_factor)
+
+        ### Crop only the part that will remain in the result (more efficient)
+        # Centered bbox of the final desired size in resized (larger/smaller) image coordinates
+        y1, x1 = max(0, new_height - height) // 2, max(0, new_width - width) // 2
+        y2, x2 = y1 + height, x1 + width
+        bbox = np.array([y1,x1,y2,x2])
+        # Map back to original image coordinates
+        bbox = (bbox / zoom_factor).astype(np.int)
+        y1, x1, y2, x2 = bbox
+        cropped_img = image[y1:y2, x1:x2]
+
+        # Handle padding when downscaling
+        resize_height, resize_width = min(new_height, height), min(new_width, width)
+        pad_height1, pad_width1 = (height - resize_height) // 2, (width - resize_width) //2
+        pad_height2, pad_width2 = (height - resize_height) - pad_height1, (width - resize_width) - pad_width1
+        pad_spec = [(pad_height1, pad_height2), (pad_width1, pad_width2)] + [(0,0)] * (image.ndim - 2)
+
+        result = cv2.resize(cropped_img, (resize_width, resize_height))
+        result = np.pad(result, pad_spec, mode='constant')
+        assert result.shape[0] == height and result.shape[1] == width
+        return result
+
+    def random_crop_image(self, image, height, width):
+        """
+            Applies random cropping to the given image 
+            Parameters
+            ----------
+                image : ndim np.array
+                    image to be cropped.
+                height : int
+                    height of the resulting cropped image. Must be <= image.shape[0]
+                width : int 
+                    width of the resulting cropped image. Must be <= image.shape[1]
+            Returns
+            -------
+                cropped image as np.array
+        """
+        assert image.shape[0] >= height
+        assert image.shape[1] >= width
+        x = np.random.randint(0, image.shape[1] - width)
+        y = np.random.randint(0, image.shape[0] - height)
+        image = image[y:y+height, x:x+width]
+        return image
